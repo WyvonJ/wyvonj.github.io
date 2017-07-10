65dd420d156df7b6d90f4f8b0d4f132eac105e1e
unknown <wyvonj@gmail.com>
2017-07-10 23:20:09 +0800

    update resume responsive design

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 (function() {
   //设定所有的tooltip
   var lg = console.log
+
+  var $root = $('body,html') //缓存，避免多次查询
+  var $backToTop = $('#back-top a')
   $(function() {
     $('[data-toggle="tooltip"]').tooltip()
   })
@@ -11,8 +14,8 @@
         width: $(this).data("level") + "%"
       })
     })
-    var year = (new Date()).getFullYear()
-    $('#year').text(year)
+    $('#year').text((new Date()).getFullYear())//footer显示年份
+    $backToTop.hide()//初始隐藏backtotop
 
   })
 
@@ -21,19 +24,29 @@
   })
 
   //滚动到页面顶部
-  $('#back-top a').click(function() {
-    $('body,html').stop(false, false).animate({
+  $backToTop.click(function() {
+    $root.stop(false, false).animate({
       scrollTop: 0
     }, 500)
     return false
   })
 
   $('a').click(function() {
-    $('html, body').animate({
+    $root.animate({
       //获取锚点的top位置 
       scrollTop: $($.attr(this, 'href')).offset().top
     }, 500)
     return false
   })
+
+
+  window.addEventListener('mousewheel',function(e){
+    if (window.scrollY>=600) {
+      $backToTop.show()
+    } else{
+      $backToTop.hide()
+    }
+  })
+
 
 })()
